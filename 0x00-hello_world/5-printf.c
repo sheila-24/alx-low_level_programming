91ae0b8dea66b1918de52fb8aef0c603c44d88b5
sheila kamau <sheilakamau0@gmail.com>
2022-06-02 16:05:08 -0700

    5th printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
+ * proper programming,
  * Return: 0 if exited properly, non-zero otherwise  
  */
 int main(void)
