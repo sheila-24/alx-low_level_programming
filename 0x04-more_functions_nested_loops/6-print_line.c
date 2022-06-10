a5b6ab8585ddd7e0f31dd2ea29aa7c7653ebb827
sheila kamau <sheilakamau0@gmail.com>
2022-06-10 13:22:18 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: parameter used
+ * Return: nothing to return here
+ */
+void print_line(int n)
+{
+	int x = 0;
+
+	if (n > 0)
+	{
+		for (; x < n; x++)
+			_putchar('_')
+	}
+	_putchar('\n');
+}
