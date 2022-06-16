fbe2abd9810d400c3bcb693578fd99f0e2cd1f84
sheila kamau <sheilakamau0@gmail.com>
2022-06-16 08:25:39 -0700

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -35,7 +35,7 @@ void print_buffer(char *b, int size)
 				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + 1));
+					printf("%c", *(b + l));
 			}
 			printf("\n");
 		}
